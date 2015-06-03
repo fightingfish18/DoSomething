74970440941c3be9f8aadb8aa97864ef4a0cff72
Henry Chipman <hdchipman@msn.com>
2015-06-03 03:52:55 -0700

    nicer list with images

--- app/src/main/java/edu/washington/wsmay1/dosomething/EventAdapter.java
+++ app/src/main/java/edu/washington/wsmay1/dosomething/EventAdapter.java
@@ -12,12 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 /**
- * Adapter to bind a ToDoItem List to a view
+ * Adapter to bind a Event List to a view
  */
 public class EventAdapter extends ArrayAdapter<Event> {
     private Activity activity;
@@ -52,6 +53,8 @@ public class EventAdapter extends ArrayAdapter<Event> {
     public static class ViewHolder {
         public TextView display_name;
         public TextView display_number;
+        public TextView display_time;
+        public ImageView display_image;
 
     }
 
@@ -64,7 +67,10 @@ public class EventAdapter extends ArrayAdapter<Event> {
                 holder = new ViewHolder();
 
                 holder.display_name = (TextView) vi.findViewById(R.id.eventName);
-                holder.display_number = (TextView) vi.findViewById(R.id.eventDescription);
+                holder.display_number = (TextView) vi.findViewById(R.id.eventDate);
+                holder.display_time = (TextView) vi.findViewById(R.id.eventTime);
+                holder.display_image = (ImageView) vi.findViewById(R.id.categoryImage);
+
 
 
                 vi.setTag(holder);
@@ -72,10 +78,31 @@ public class EventAdapter extends ArrayAdapter<Event> {
                 holder = (ViewHolder) vi.getTag();
             }
 
-
+            final String category = events.get(position).getCategory().toLowerCase().trim();
 
             holder.display_name.setText(events.get(position).getName());
-            holder.display_number.setText(events.get(position).getDescription());
+            holder.display_number.setText(events.get(position).getDate());
+            holder.display_time.setText(events.get(position).getTime()+" on ");
+
+            if (category.equals("athletics")) {
+                holder.display_image.setImageResource(R.drawable.sport);
+            } else if (category.equals("academics")) {
+                holder.display_image.setImageResource(R.drawable.book);
+            } else if (category.equals("social")) {
+                holder.display_image.setImageResource(R.drawable.network);
+            } else if (category.equals("night-life")) {
+                holder.display_image.setImageResource(R.drawable.moon);
+            } else if (category.equals("gaming")) {
+                holder.display_image.setImageResource(R.drawable.game);
+            } else if (category.equals("entertainment")) {
+                holder.display_image.setImageResource(R.drawable.tele);
+            } else if (category.equals("activism")) {
+                holder.display_image.setImageResource(R.drawable.mega);
+            } else if (category.equals("party")) {
+                holder.display_image.setImageResource(R.drawable.balloon);
+            } else if (category.equals("other")) {
+                holder.display_image.setImageResource(R.drawable.rsz_ban );
+            }
 
 
         } catch (Exception e) {
