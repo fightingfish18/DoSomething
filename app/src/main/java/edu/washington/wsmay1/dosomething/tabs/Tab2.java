d11998941e7748575c01c2d08f2d36105d04db06
Wesley Wolanski <wgwolanski@gmail.com>
2015-06-02 20:27:55 -0700

    All tab functionality should be there. Content needs to be added. Classes might need to be moved.

---
+++ app/src/main/java/edu/washington/wsmay1/dosomething/tabs/Tab2.java
@@ -0,0 +1,25 @@
+package edu.washington.wsmay1.dosomething.tabs;
+
+/**
+ * Created by Slee on 6/2/2015.
+ */
+
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import edu.washington.wsmay1.dosomething.R;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
